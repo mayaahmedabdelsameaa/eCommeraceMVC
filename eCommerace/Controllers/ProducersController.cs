0ae1ef30a362b8eb1a15d0ac8877fbdf603c7616
mayaahmedabdelsameaa <mayarahmed8624@gmail.com>
2023-05-05 17:58:37 +0300

    add Producer views

--- eCommerace/Controllers/ProducersController.cs
+++ eCommerace/Controllers/ProducersController.cs
@@ -1,4 +1,6 @@
 ﻿using eCommerace.Data;
+using eCommerace.Data.Services;
+using eCommerace.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,15 +8,84 @@ namespace eCommerace.Controllers
 {
     public class ProducersController : Controller
     {
-        private readonly AppDBContext _context;
-        public ProducersController(AppDBContext context)
+        private readonly IProducersService _service;
+        public ProducersController(IProducersService service)
         {
-            _context = context;
+            _service = service;
         }
         public async Task<IActionResult> Index()
         {
-            var data = await _context.Producers.ToListAsync();
-            return View(data);
+            var allProducers = await _service.GettAll();
+            return View(allProducers);
+        }
+        // Get: Producer/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // Post: Producer/Create 
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ProfilePicture,FullName,Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(producer);
+            }
+            await _service.Add(producer);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get: Producer/details/id
+        public async Task<IActionResult> Details(int id)
+        {
+            var producerDetails = await _service.GetByID(id);
+            if (producerDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(producerDetails);
+        }
+
+        // Get: Producer/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var producerDetails = await _service.GetByID(id);
+            if (producerDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(producerDetails);
+        }
+        // Post: Producer/Edit/1 
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProfilePicture,FullName,Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(producer);
+            }
+            await _service.Update(id, producer);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Get: Producer/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var producerDetails = await _service.GetByID(id);
+            if (producerDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(producerDetails);
+        }
+        // Post: Producer/Delete/1 
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var producerDeatils = await _service.GetByID(id);
+            if (producerDeatils == null) return View("NotFound");
+            await _service.Delete(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
